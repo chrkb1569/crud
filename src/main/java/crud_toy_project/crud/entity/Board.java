18aabb88943d3f3b72f26e04225c4a9891bc8d88
chrkb1569 <chrkb1569@naver.com>
2022-07-07 01:06:05 +0900

    style : renamed board

--- src/main/java/crud_toy_project/crud/entity/Board.java
+++ src/main/java/crud_toy_project/crud/entity/Board.java
@@ -9,13 +9,13 @@ import javax.persistence.Id;
 
 @Entity
 @Data
-public class Board {
+public class board {
     // Mysql에서 데이터를 설정해 줄 때, number값을 PK로 설정해두었으므로,
     // number변수를 PK로 사용한다는 의미에서 @Id 선언
     @Id
     // 키가 생성되는 방식을 정의함. strategy = GenerationType.AUTO의 경우에는
     // 데이터 베이스가 자동적으로 키를 생성한다는 것을 나타냄
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer number; // 게시물의 번호
     private String title; // 게시물의 제목
     private String content; // 게시물의 내용
