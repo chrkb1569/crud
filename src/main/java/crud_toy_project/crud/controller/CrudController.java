8cf8e0ed45fbe0604e6032e8f48cf94bf5e4aa4e
chrkb1569 <chrkb1569@naver.com>
2022-07-07 01:09:44 +0900

    fix : fix the parameter error

--- src/main/java/crud_toy_project/crud/controller/CrudController.java
+++ src/main/java/crud_toy_project/crud/controller/CrudController.java
@@ -1,11 +1,17 @@
 package crud_toy_project.crud.controller;
 
+import crud_toy_project.crud.entity.board;
+import crud_toy_project.crud.service.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller// 해당 클래스가 Controller임을 표기
 public class CrudController {
+    @Autowired
+    private CrudService crudService;
 
     @RequestMapping
     public String write_form() {
@@ -13,8 +19,11 @@ public class CrudController {
     }
 
     @PostMapping("/board/write")
-    public String write() {
-        return "";
+    public String write(board board, Model model) {
+        crudService.save(board);
+        model.addAttribute("board", board);
+
+        return "writeresultform";
     }
 
 }
--- src/main/java/crud_toy_project/crud/service/CrudService.java
+++ src/main/java/crud_toy_project/crud/service/CrudService.java
@@ -1,11 +1,17 @@
 package crud_toy_project.crud.service;
 
+import crud_toy_project.crud.entity.board;
+import crud_toy_project.crud.repository.CrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class CrudService {
+    @Autowired
+    private CrudRepository crudRepository;
 
+    public void save(board board) {
+     crudRepository.save(board);
+    }
 
 }
